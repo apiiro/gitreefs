eb5f11d209ae5d30ec0c933cea37a8ac9d9d5406
Liri Sokol <liri@apiiro.com>
2021-03-21 13:40:20 +0200

    no need to switch goos

--- test_utils/exec.go
+++ test_utils/exec.go
@@ -1,10 +1,8 @@
 package testutils
 
 import (
-	"fmt"
 	"gitreefs/core/logger"
 	"os/exec"
-	"runtime"
 )
 
 func runCommand(cmd *exec.Cmd) {
@@ -34,12 +32,5 @@ func ExecCommandWithDir(dir string, name string, arg ...string) {
 }
 
 func NfsMount(mountPoint string) {
-	switch runtime.GOOS {
-	case "linux":
-		ExecCommand("mount", "-o", "port=2049,mountport=2049", "-t", "nfs", "localhost:/", mountPoint)
-	case "darwin":
-		ExecCommand("mount", "-o", "port=2049,mountport=2049,nfsvers=3,noacl,tcp", "-t", "nfs", "localhost:/", mountPoint)
-	default:
-		panic(fmt.Sprintf("unsupported os: %v", runtime.GOOS))
-	}
+	ExecCommand("mount", "-o", "port=2049,mountport=2049,nfsvers=3,noacl,tcp", "-t", "nfs", "localhost:/", mountPoint)
 }
